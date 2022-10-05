ebf33f4c3805379adbf580e1141763c8c028d6e6
Elena <elena1709@yandex.ru>
2022-10-05 13:17:42 +0300

    commit for hw

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string? num = string.Empty;
+do
+{
+    num = Console.ReadLine();
+} 
+while (num.Length < 3 || num.Length > 3);
+
+Console.WriteLine($"Вторая цифра числа {num} = {num[1]}");
